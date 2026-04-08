e794675972c2a85dd000da59afad8bb5019d297c
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2026-04-09 00:20:53 +0600

    Add SVG logos for Airbnb, Booking, Tripadvisor, and Vrbo in HeroSection
    - Created AirbnbLogo component with SVG representation of Airbnb logo.
    - Created BookingLogo component with SVG representation of Booking logo.
    - Created TripadvisorLogo component with SVG representation of Tripadvisor logo.
    - Created VrboLogo component with SVG representation of Vrbo logo.

--- src/components/modules/LandingPage/HeroSection.tsx
+++ src/components/modules/LandingPage/HeroSection.tsx
@@ -1,5 +1,9 @@
 import { Button } from "@/components/ui/button";
 import { RightArrow } from "@/svg/Header/RightArrow";
+import { AirbnbLogo } from "@/svg/HeroSection/AirbnbLogo";
+import { BookingLogo } from "@/svg/HeroSection/BookingLogo";
+import { TripadvisorLogo } from "@/svg/HeroSection/Tripadvisor";
+import { VrboLogo } from "@/svg/HeroSection/VrboLogo";
 import React from "react";
 
 export default function HeroSection() {
@@ -35,35 +39,83 @@ export default function HeroSection() {
             </h6>
             <div className="hidden md:block">
                 <div
-                    className="h-3.25 w-3.25 rounded-full bg-[#635BFF] absolute top-35 left-30"
-                    style={{ boxShadow: "inset 0 1px 3.3px 0 #00000040" }}
+                    className="h-3.25 w-3.25 rounded-full bg-[#635BFF] absolute top-35 left-30 animate-float"
+                    style={{
+                        boxShadow: "inset 0 1px 3.3px 0 #00000040",
+                        animationDuration: "6s",
+                    }}
                 ></div>
                 <div
-                    className="h-3.25 w-3.25 rounded-full bg-[#FF5A5F] absolute top-90 left-55"
-                    style={{ boxShadow: "inset 0 1px 3.3px 0 #00000040" }}
+                    className="h-3.25 w-3.25 rounded-full bg-[#FF5A5F] absolute top-90 left-55 animate-float"
+                    style={{
+                        boxShadow: "inset 0 1px 3.3px 0 #00000040",
+                        animationDuration: "5s",
+                    }}
                 ></div>
                 <div
-                    className="h-1.75 w-1.75 rounded-full bg-[#FFEC5A] absolute bottom-10 left-30"
-                    style={{ boxShadow: "inset 0 1px 2.3px 0 #00000040" }}
+                    className="h-1.75 w-1.75 rounded-full bg-[#FFEC5A] absolute bottom-10 left-30 animate-float"
+                    style={{
+                        boxShadow: "inset 0 1px 2.3px 0 #00000040",
+                        animationDuration: "4.5s",
+                    }}
                 ></div>
                 <div
-                    className="h-3.75 w-3.75 rounded-full bg-[#34E0A1] absolute md:top-25 md:left-120 lg:top-25 lg:left-180"
-                    style={{ boxShadow: "inset 2px -2px 3.3px 0px #00000040" }}
+                    className="h-3.75 w-3.75 rounded-full bg-[#34E0A1] absolute md:top-25 md:left-120 lg:top-25 lg:left-180 animate-float"
+                    style={{
+                        boxShadow: "inset 2px -2px 3.3px 0px #00000040",
+                        animationDuration: "3.5s",
+                    }}
                 ></div>
                 <div
-                    className="h-3.25 w-3.25 rounded-full bg-[#5AF9FF] absolute md:bottom-10 md:left-100 lg:bottom-10 lg:left-195"
-                    style={{ boxShadow: "inset 2px 2px 3.3px 0 #00000040" }}
+                    className="h-3.25 w-3.25 rounded-full bg-[#5AF9FF] absolute md:bottom-10 md:left-100 lg:bottom-10 lg:left-195 animate-float"
+                    style={{
+                        boxShadow: "inset 2px 2px 3.3px 0 #00000040",
+                        animationDuration: "5s",
+                    }}
                 ></div>
 
                 <div
-                    className="h-3.25 w-3.25 rounded-full bg-[#FFEC5A] absolute md:top-60 md:right-20 lg:top-50 lg:right-60"
-                    style={{ boxShadow: "inset 0 1px 2.3px 0 #00000040" }}
+                    className="h-3.25 w-3.25 rounded-full bg-[#FFEC5A] absolute md:top-60 md:right-20 lg:top-50 lg:right-60 animate-float"
+                    style={{
+                        boxShadow: "inset 0 1px 2.3px 0 #00000040",
+                        animationDuration: "4.5s",
+                    }}
                 ></div>
 
                 <div
-                    className="h-1.75 w-1.75 rounded-full bg-[#0C3B7C] absolute bottom-10 right-40"
-                    style={{ boxShadow: "inset 0 1px 2.3px 0 #00000040" }}
+                    className="h-1.75 w-1.75 rounded-full bg-[#0C3B7C] absolute bottom-10 right-40 animate-float"
+                    style={{
+                        boxShadow: "inset 0 1px 2.3px 0 #00000040",
+                        animationDuration: "3s",
+                    }}
                 ></div>
+            </div>
+
+            <div className="hidden md:block">
+                <div
+                    className="absolute top-40 left-5 lg:top-20 lg:left-70 animate-float"
+                    style={{ animationDuration: "4s" }}
+                >
+                    <AirbnbLogo />
+                </div>
+                <div
+                    className="absolute top-105 left-30 lg:top-100 lg:left-90 animate-float"
+                    style={{ animationDuration: "5s" }}
+                >
+                    <VrboLogo />
+                </div>
+                <div
+                    className="absolute top-20 right-5 lg:top-25 lg:right-80 animate-float"
+                    style={{ animationDuration: "3.5s" }}
+                >
+                    <BookingLogo />
+                </div>
+                <div
+                    className="absolute top-105 right-5 lg:top-100 lg:right-90 animate-float"
+                    style={{ animationDuration: "4.5s" }}
+                >
+                    <TripadvisorLogo />
+                </div>
             </div>
         </div>
     );
