66cd2489e252bdcdd7835908be423889a4b3b56f
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2026-04-10 21:49:58 +0600

    fix: adjust styles for GettingStartedCard component for better alignment

--- src/components/modules/LandingPage/GettingStartedCard.tsx
+++ src/components/modules/LandingPage/GettingStartedCard.tsx
@@ -9,7 +9,7 @@ export default function GettingStartedCard({
     return (
         <div>
             <div className="relative border-2 border-[#FDEBF0] bg-white hover:shadow-[0_4px_90.3px_0_rgba(215,205,207,0.47)] min-w-71.25 min-h-48 rounded-[9px] flex items-center justify-center">
-                <div className="text-[29.23px] font-medium h-19 w-19 rounded-full flex items-center justify-center border-[5px] border-[#ED3C6A] absolute -top-6.5 -left-6.5 bg-white">
+                <div className="text-[25px] md:text-[29.23px] font-medium h-15 w-15 md:h-19 md:w-19 rounded-full flex items-center justify-center border-[5px] border-[#ED3C6A] absolute -top-3 -left-3 md:-top-6.5 md:-left-6.5 bg-white">
                     {listItem.id}
                 </div>
                 <div>{listItem.icon}</div>
